94229948011639c519b820c560f102e581706b50
piyush1009 <piyushkhandelwal0910@gmail.com>
2020-01-26 19:59:34 +0530

    Changed data

---
+++ Geeksforgeeks/Sudo placement/Power of 2.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+ 
+#define ll          long long
+#define pb          push_back
+#define pii         pair<int,int> 
+#define vi          vector<int>
+#define vii         vector<pii>
+#define mi          map<int,int>
+#define mii         map<pii,int>
+#define all(a)      (a).begin(),(a).end()
+#define F         first
+#define S           second
+#define sz(x)       (int)x.size()
+#define endl        '\n'
+#define hell        1000000007
+#define what_is(x)  cerr << #x << " is " << x << endl;
+#define rep(i,a,b)  for(int i=a;i<b;i++)
+using namespace std;
+ 
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        unsigned long long int s;
+        int flag=0;
+        cin>>s;
+        long double q=log10(s)/log10(2);
+        long long int f=(long long int)q;
+        if(f==q)
+        {
+            cout<<"YES"<<endl;
+        }
+        else
+        {
+            cout<<"NO"<<endl;
+        }
+    }
+    
+    return 0;
+}
