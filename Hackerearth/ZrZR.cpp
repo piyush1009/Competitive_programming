5c71c191645c64912c5044fdb2960a2e0d9e9ff5
piyush1009 <piyushkhandelwal0910@gmail.com>
2020-01-30 01:24:53 +0530

    Changed data

---
+++ Hackerearth/ZrZR.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+ 
+#define ll          long long
+#define pb          push_back
+#define pii         pair<int,int> 
+#define vi          vector<int>
+#define vii         vector<pii>
+#define mi          map<int,int>
+#define mii         map<pii,int>
+#define all(a)      (a).begin(),(a).end()
+#define F         first
+#define S           second
+#define sz(x)       (int)x.size()
+#define endl        '\n'
+#define hell        1000000007
+#define what_is(x)  cerr << #x << " is " << x << endl;
+#define rep(i,a,b)  for(int i=a;i<b;i++)
+using namespace std;
+ 
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        ll n,i,sum=0;
+        cin>>n;
+        for(i=5;n/i>=1;i=i*5)
+        {
+            sum=sum+n/i;
+        }
+        cout<<sum<<endl;
+    }
+    return 0;
+}
